59a9b51d12c74dbe9436445657cbd51135c7fc04
Stephen Chryn <SRCthird@gmail.com>
2024-05-13 19:16:51 -0400

    Add create and update structs

--- src/models.rs
+++ src/models.rs
@@ -14,11 +14,41 @@ pub struct Inventory {
 }
 
 #[derive(Queryable)]
+pub struct CreateInventory {
+    pub lot_number: String,
+    pub location: String,
+    pub quantity: i32,
+    pub created_by: String,
+}
+
+#[derive(Queryable)]
+pub struct UpdateInventory {
+    pub lot_number: Option<String>,
+    pub location: Option<String>,
+    pub quantity: Option<i32>,
+    pub updated_by: String
+}
+
+#[derive(Queryable)]
 pub struct InventoryBay {
     pub id: i32,
     pub name: String,
     pub warehouse_name: String,
     pub max_unique_lots: i32,
+}
+
+#[derive(Queryable)]
+pub struct CreateInventoryBay {
+    pub name: String,
+    pub warehouse_name: String,
+    pub max_unique_lots: i32,
+}
+
+#[derive(Queryable)]
+pub struct UpdateInventoryBay {
+    pub name: Option<String>,
+    pub warehouse_name: Option<String>,
+    pub max_unique_lots: Option<i32>,
 }
 
 #[derive(Queryable)]
@@ -34,10 +64,42 @@ pub struct Log {
 }
 
 #[derive(Queryable)]
+pub struct CreateLog {
+    pub from_location: String,
+    pub to_location: String,
+    pub user: String,
+    pub lot_number: String,
+    pub quantity_moved: i32,
+    pub comments: String,
+}
+
+#[derive(Queryable)]
+pub struct UpdateLog {
+    pub from_location: Option<String>,
+    pub to_location: Option<String>,
+    pub user: Option<String>,
+    pub lot_number: Option<String>,
+    pub quantity_moved: Option<i32>,
+    pub comments: Option<String>,
+}
+
+#[derive(Queryable)]
 pub struct Product {
     pub id: i32,
     pub name: String,
     pub description: String,
+}
+
+#[derive(Queryable)]
+pub struct CreateProduct {
+    pub name: String,
+    pub description: String,
+}
+
+#[derive(Queryable)]
+pub struct UpdateProduct {
+    pub name: Option<String>,
+    pub description: Option<String>,
 }
 
 #[derive(Queryable)]
@@ -47,6 +109,22 @@ pub struct ProductLot {
     pub internal_reference: String,
     pub product_name: String,
     pub quantity: i32,
+}
+
+#[derive(Queryable)]
+pub struct CreateProductLot {
+    pub lot_number: String,
+    pub internal_reference: String,
+    pub product_name: String,
+    pub quantity: i32,
+}
+
+#[derive(Queryable)]
+pub struct UpdateProductLot {
+    pub lot_number: Option<String>,
+    pub internal_reference: Option<String>,
+    pub product_name: Option<String>,
+    pub quantity: Option<i32>,
 }
 
 #[derive(Queryable)]
@@ -65,7 +143,36 @@ pub struct User {
 }
 
 #[derive(Queryable)]
+pub struct CreateUser {
+    pub email: String,
+    pub password: String,
+    pub role: String,
+    pub position: Option<String>,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub bio: Option<String>,
+    pub image: Option<String>,
+}
+
+#[derive(Queryable)]
+pub struct UpdateUser {
+    pub email: Option<String>,
+    pub password: Option<String>,
+    pub role: Option<String>,
+    pub position: Option<String>,
+    pub first_name: Option<String>,
+    pub last_name: Option<String>,
+    pub bio: Option<String>,
+    pub image: Option<String>,
+}
+
+#[derive(Queryable)]
 pub struct Warehouse {
     pub id: i32,
+    pub name: String,
+}
+
+#[derive(Queryable)]
+pub struct CreateOrUpdateWarehouse {
     pub name: String,
 }
