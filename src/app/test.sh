77a1f664c4517911b5f09d2b50ba3aeb9a5323b7
Stephen Chryn <SRCthird@gmail.com>
2024-05-13 17:22:24 -0400

    Desided to go with Rust instead of C++

--- src/app/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Base URL of your Crow application
-BASE_URL="http://localhost:18080/"
-
-# Test home page connection
-echo "Testing home page connection:"
-curl "$BASE_URL"
-echo -e "\n"
