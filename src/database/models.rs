8235808e49421d42f46d297f3dd93eb1ef1181dc
Stephen Chryn <SRCthird@gmail.com>
2024-05-14 13:59:55 -0400

    Move models and schema to database/

--- src/database/models.rs
+++ src/database/models.rs
@@ -1,7 +1,8 @@
 use diesel::prelude::*;
 use chrono::NaiveDateTime;
 
-#[derive(Queryable)]
+#[derive(Queryable, Debug, AsChangeset)]
+#[diesel(table_name = crate::database::schema::inventory)]
 pub struct Inventory {
     pub id: i32,
     pub lot_number: String,
@@ -13,20 +14,24 @@ pub struct Inventory {
     pub updated_by: Option<String>,
 }
 
-#[derive(Queryable)]
-pub struct CreateInventory {
-    pub lot_number: String,
-    pub location: String,
+#[derive(Insertable)]
+#[diesel(table_name = crate::database::schema::inventory)]
+pub struct CreateInventory<'a> {
+    pub lot_number: &'a str,
+    pub location: &'a str,
     pub quantity: i32,
-    pub created_by: String,
+    pub created_by: &'a str,
+    pub created_at: NaiveDateTime,
 }
 
-#[derive(Queryable)]
+#[derive(AsChangeset)]
+#[diesel(table_name = crate::database::schema::inventory)]
 pub struct UpdateInventory {
     pub lot_number: Option<String>,
     pub location: Option<String>,
     pub quantity: Option<i32>,
-    pub updated_by: String
+    pub updated_by: String,
+    pub updated_at: NaiveDateTime,
 }
 
 #[derive(Queryable)]
