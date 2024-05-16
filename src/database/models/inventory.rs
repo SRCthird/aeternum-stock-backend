31c92393c4ed3c4532a8d00d21a49cee8347edc3
Stephen Chryn <SRCthird@gmail.com>
2024-05-16 16:49:21 -0400

    Add Request structs to inventory

--- src/database/models/inventory.rs
+++ src/database/models/inventory.rs
@@ -2,7 +2,7 @@ use diesel::prelude::*;
 use rocket::serde::{Deserialize, Serialize};
 use chrono::NaiveDateTime;
 
-#[derive(Serialize, Queryable)]
+#[derive(Debug, Serialize, Queryable)]
 #[serde(crate = "rocket::serde")]
 pub struct Inventory {
     pub id: i32,
@@ -15,16 +15,41 @@ pub struct Inventory {
     pub updated_by: Option<String>,
 }
 
+#[derive(Debug, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+pub struct RequestCreateInventory {
+    pub lot_number: String,
+    pub location: String,
+    pub quantity: i32,
+    pub created_by: String,
 
-#[derive(Insertable, Deserialize)]
+    pub from_location: String,
+    pub comment: String,
+}
+
+
+#[derive(Debug, Insertable, Deserialize)]
 #[serde(crate = "rocket::serde")]
 #[diesel(table_name = crate::database::schema::inventory)]
-pub struct CreateInventory<'a> {
-    pub lot_number: &'a str,
-    pub location: &'a str,
+pub struct CreateInventory {
+    pub lot_number: String,
+    pub location: String,
     pub quantity: i32,
-    pub created_by: &'a str,
-    pub created_at: NaiveDateTime,
+    pub created_by: String,
+    pub created_at: Option<NaiveDateTime>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+pub struct RequestUpdateInventory {
+    pub lot_number: Option<String>,
+    pub location: Option<String>,
+    pub quantity: Option<i32>,
+    pub updated_by: String,
+    pub updated_at: NaiveDateTime,
+
+    pub from_location: Option<String>,
+    pub comment: Option<String>,
 }
 
 #[derive(Deserialize, AsChangeset, Debug)]
